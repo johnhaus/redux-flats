75da34fd1f2ca200844c223920dc7812bcde7570
johnhaus <john.haus@outlook.com>
2019-03-30 21:11:04 -0300

    added some styling

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Flatlist extends Component {
   render () {
     return (
-      <div className="col-sm-7">
+      <div className="flat-list col-sm-7">
         Flat List
       </div>
     );
