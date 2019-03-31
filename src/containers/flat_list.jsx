db975fe1cff3e1d70b0b9f0b62921a32b1d21922
johnhaus <john.haus@outlook.com>
2019-03-30 22:26:49 -0300

    added flat and styling

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import Flat from '../components/flat';
+
 class Flatlist extends Component {
+  static defaultProps = {
+    flats: [{
+      "name": "Charm at the Steps of Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      "price": 164,
+      "priceCurrency": "EUR"
+    }]
+  }
+
   render () {
     return (
       <div className="flat-list col-sm-7">
-        Flat List
+        {this.props.flats.map((flat) => <Flat flat={flat} key={flat.name} />)}
       </div>
     );
   }
